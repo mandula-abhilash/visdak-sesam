98b02cff20c487f51ec8d008695863cb9b8d13fe
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-19 14:49:48 +0530

    Load environment variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 import { authRouter } from "./routes/auth.routes.js";
 import { protect, admin } from "./middleware/auth.middleware.js";
