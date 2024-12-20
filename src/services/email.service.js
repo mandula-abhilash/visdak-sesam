30cb1adf66db5bbec8c9e71ad25cc5a2ec36cbaf
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-20 15:46:44 +0530

    Improve email error handling

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -17,18 +17,24 @@ const sesClient = new SESClient({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
+  apiVersion: process.env.AWS_API_VERSION,
 });
 
 const sendEmail = async (to, subject, html) => {
-  const command = new SendEmailCommand({
-    Source: process.env.SES_EMAIL_FROM,
-    Destination: { ToAddresses: [to] },
-    Message: {
-      Subject: { Data: subject },
-      Body: { Html: { Data: html } },
-    },
-  });
-  await sesClient.send(command);
+  try {
+    const command = new SendEmailCommand({
+      Source: process.env.SES_EMAIL_FROM,
+      Destination: { ToAddresses: [to] },
+      Message: {
+        Subject: { Data: subject },
+        Body: { Html: { Data: html } },
+      },
+    });
+    await sesClient.send(command);
+  } catch (error) {
+    console.error("Email sending error:", error);
+    throw new Error("Failed to send email.");
+  }
 };
 
 export const emailService = {
