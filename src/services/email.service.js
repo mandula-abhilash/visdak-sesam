0ae5ad6ffb9bd94512a6d45b6dc83588c81eda07
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-20 15:57:59 +0530

    Fix AWS SES client configuration

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -12,11 +12,9 @@ const defaultTemplates = {
 let customTemplates = { ...defaultTemplates };
 
 const sesClient = new SESClient({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
   apiVersion: process.env.AWS_API_VERSION,
 });
 
